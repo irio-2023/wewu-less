d1693dd690633a105d17b50819071fd012a1defd
Przemysław Fuchs <fuchs.przemyslaw@gmail.com>
2024-01-12 01:24:26 +0100

    WEWU: New PubSub handling

--- terraform/queue_template.tf
+++ terraform/queue_template.tf
@@ -1,6 +1,6 @@
 resource "google_pubsub_topic" "pubsub" {
-  for_each = local.queues
-  name = each.value.topic
+  for_each = local.terraform_queues
+  name = each.value.name
   message_retention_duration = each.value.message_retention
   project = local.gcp_project
 }
--- terraform/queues.tf
+++ terraform/queues.tf
@@ -1,8 +1,14 @@
 locals {
-  queues = {
+  terraform_queues = {
     register_service_task_queue = {
-      topic = "register-service-task-queue"
+      name = "register-service-task-queue"
       message_retention = "86600s"
     }
+  }
+
+  queues = { for k, v in local.terraform_queues :
+    k => merge(v, {
+      topic = google_pubsub_topic.pubsub[k].id
+    })
   }
 }
