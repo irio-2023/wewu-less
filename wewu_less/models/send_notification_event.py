ed4ac21eb55dc4722428d2dce4554f864ceaf803
Szymon Potrzebowski <szymonp1806@gmail.com>
2024-01-17 22:13:10 +0100

    fixed tests

--- wewu_less/models/send_notification_event.py
+++ wewu_less/models/send_notification_event.py
@@ -1,17 +1,18 @@
 import uuid
 from dataclasses import dataclass
+from typing import Optional
 
 from wewu_less.models.service_admin import ServiceAdmin
 
 
 @dataclass
 class SendNotificationEvent:
-    notification_id: uuid.UUID
     job_id: uuid.UUID
     primary_admin: ServiceAdmin
     secondary_admin: ServiceAdmin
     ack_timeout_secs: int
-    escalation_number: int
+    notification_id: Optional[uuid.UUID] = None
+    escalation_number: Optional[int] = None
 
     def __post_init__(self):
         if isinstance(self.primary_admin, dict):
