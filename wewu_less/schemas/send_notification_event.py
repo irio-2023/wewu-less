5d261688819d5b5f57e37ad617bf41a592238c27
Szymon Potrzebowski <szymonp1806@gmail.com>
2024-01-17 22:02:27 +0100

    format

--- wewu_less/schemas/send_notification_event.py
+++ wewu_less/schemas/send_notification_event.py
@@ -4,9 +4,17 @@ from wewu_less.schemas.service_admin import ServiceAdminSchema
 
 
 class SendNotificationEventSchema(Schema):
-    notification_id = fields.UUID(required=False, default=None, data_key="notificationId")
+    notification_id = fields.UUID(
+        required=False, default=None, data_key="notificationId"
+    )
     job_id = fields.UUID(required=True, data_key="jobId")
-    primary_admin = fields.Nested(ServiceAdminSchema, required=True, data_key="primaryAdmin")
-    secondary_admin = fields.Nested(ServiceAdminSchema, required=True, data_key="secondaryAdmin")
+    primary_admin = fields.Nested(
+        ServiceAdminSchema, required=True, data_key="primaryAdmin"
+    )
+    secondary_admin = fields.Nested(
+        ServiceAdminSchema, required=True, data_key="secondaryAdmin"
+    )
     ack_timeout_secs = fields.Integer(required=True, data_key="ackTimeoutSecs")
-    escalation_number = fields.Integer(required=True, data_key="escalationNumber", default=0)
+    escalation_number = fields.Integer(
+        required=True, data_key="escalationNumber", default=0
+    )
