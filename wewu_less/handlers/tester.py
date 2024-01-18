0031072cccd51cccb2c749078bf795a3a3b55b07
Przemysław Fuchs <fuchs.przemyslaw@gmail.com>
2024-01-18 19:31:46 +0100

    WEWU: Fix return value

--- wewu_less/handlers/tester.py
+++ wewu_less/handlers/tester.py
@@ -14,16 +14,16 @@ def wewu_tester(request: flask.Request):
         try:
             document = tester_client_collection.find_one()
             status = document["status_code"]
-            return status
+            return {}, status
         except Exception:
             logger.exception(
                 "Failed to retrieve tester record from database, returning 200"
             )
-            return 200
+            return {}, 200
     elif request.method == "POST":
         body = request.get_json()
         new_status_code = body["status_code"]
         tester_client_collection.update_many(
             {}, {"$set": {"status_code": int(new_status_code)}}, upsert=True
         )
-        return 200
+        return {}, 200
