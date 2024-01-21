6f2630e3c388a4158de84ec44ad8531ce32f4d8f
Szymon Potrzebowski <szymonp1806@gmail.com>
2024-01-21 16:45:24 +0100

    format >:[

--- tests/manual/testscenario.py
+++ tests/manual/testscenario.py
@@ -1,20 +1,21 @@
-import requests
 import json
-import time
 import random
+import time
+
+import requests
 
 mail1 = "youremailgoeshere@service.com"
 mail2 = "yoursecondemailgoeshere@service.com"
 
 google = {
-  "serviceUrl": "https://google.com",
-  "geoRegions": ["us-east1-b"],
-  "primaryAdmin": {"email": mail1},
-  "secondaryAdmin": {"email": mail2},
-  "pollFrequencySecs": 10,
-  "alertingWindowNumberOfCalls": 10,
-  "alertingWindowCallsFailCount": 4,
-  "ackTimeout": 60
+    "serviceUrl": "https://google.com",
+    "geoRegions": ["us-east1-b"],
+    "primaryAdmin": {"email": mail1},
+    "secondaryAdmin": {"email": mail2},
+    "pollFrequencySecs": 10,
+    "alertingWindowNumberOfCalls": 10,
+    "alertingWindowCallsFailCount": 4,
+    "ackTimeout": 60,
 }
 
 amazon = {
@@ -25,7 +26,7 @@ amazon = {
     "pollFrequencySecs": 10,
     "alertingWindowNumberOfCalls": 10,
     "alertingWindowCallsFailCount": 4,
-    "ackTimeout": 60
+    "ackTimeout": 60,
 }
 
 facebook = {
@@ -36,7 +37,7 @@ facebook = {
     "pollFrequencySecs": 8,
     "alertingWindowNumberOfCalls": 10,
     "alertingWindowCallsFailCount": 4,
-    "ackTimeout": 60
+    "ackTimeout": 60,
 }
 
 youtube = {
@@ -47,7 +48,7 @@ youtube = {
     "pollFrequencySecs": 15,
     "alertingWindowNumberOfCalls": 10,
     "alertingWindowCallsFailCount": 4,
-    "ackTimeout": 60
+    "ackTimeout": 60,
 }
 
 
@@ -61,20 +62,20 @@ BATCH_SIZE = 15
 BATCH_TIME_SEC = 6
 INITIAL_SIZE = 200
 
-headers = {'Content-type': 'application/json'}
+headers = {"Content-type": "application/json"}
 
 for i in range(INITIAL_SIZE):
-    service = random.randint(0, services_count-1)
+    service = random.randint(0, services_count - 1)
     response = requests.post(api_url, data=services_data[service], headers=headers)
-    if (response.status_code != 200):
+    if response.status_code != 200:
         print(response.text)
 
 time.sleep(120)
 for i in range(BATCH_COUNT):
     for j in range(BATCH_SIZE):
-        service = random.randint(0, services_count-1)
+        service = random.randint(0, services_count - 1)
         response = requests.post(api_url, data=services_data[service], headers=headers)
-        if (response.status_code != 200):
+        if response.status_code != 200:
             print(response.text)
     print(f"Batch {i+1} done")
     time.sleep(BATCH_TIME_SEC)
